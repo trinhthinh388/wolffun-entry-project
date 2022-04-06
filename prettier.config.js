6da98a2783952107bf3d05adfccd09e9015e6006
thinh.trinh <thinhtd@wolffungame.com>
2022-04-06 09:53:12 +0700

    feat: added linter

--- prettier.config.js
+++ prettier.config.js
@@ -2,7 +2,7 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'es5',
   bracketSpacing: true,
-  jsxBracketSameLine: false,
+  bracketSameLine: false,
   printWidth: 80,
   arrowParens: 'avoid',
   tabWidth: 2,
