a5e14c3418cc7d389309879051bc8be3a2d3575b
thinh.trinh <thinhtd@wolffungame.com>
2022-04-09 09:41:55 +0700

    feat: created footer component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,9 @@
 export { default as Header } from './Header';
 export type { HeaderProps } from './Header';
 
+export { default as Footer } from './Footer';
+export type { FooterProps } from './Footer';
+
 export { default as Button } from './Button';
 export type { ButtonProps } from './Button';
 
